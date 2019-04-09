ca7f7539c3aac3e3aae67a594cca013afd5b3c30
Dave Cornelius <d_cornelius94@yahoo.com>
2019-04-09 10:39:34 -0700

    made first tcp server

---
+++ copyStream.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const rs = fs.createReadStream('./1_streams.md', { encoding: 'utf8' });
+const ws = fs.createWriteStream('copy.md', { encoding: 'utf8' });
+
+rs
+  .pipe(ws);
+
+// rs.on('data', data => {
+//   ws.write(data);
+// });
+
+// rs.on('end', () => {
+//   ws.end();
+// });
